970d670e40085449392d90d27d8c8e3ffb36f6d5
Jani Mikkonen <jani@mikkonen.info>
2015-03-30 00:22:10 +0300

    Update web conf

--- config/web.php
+++ config/web.php
@@ -54,13 +54,24 @@ $config = [
         ],
         'db' => require(__DIR__ . '/db.php'),
     ],
+    'modules' => [
+        'ycm' => [
+            'class' => 'janisto\ycm\Module',
+            'admins' => ['admin'],
+            'urlPrefix' => 'admin',
+            'registerModels' => [
+                'basic1' => 'app\models\Basic',
+                'basic2' => 'app\models\BasicSearch',
+            ],
+        ],
+    ],
     'params' => $params,
 ];
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    //$config['bootstrap'][] = 'debug';
+    //$config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
